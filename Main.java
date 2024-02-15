6a27208e675d61eddaff4f1c6df9a351af3a63a4
tirsoalc <tirsonetogz@gmail.com>
2024-02-15 20:30:29 -0300

    added class tirsoalc

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        tirsoalc tirso = new tirsoalc();
+        System.out.println(tirso);
+    }
+}
---
+++ tirsoalc.java
@@ -0,0 +1,14 @@
+public class tirsoalc {
+    final public String nome = "Tirso Alcazar Neto";
+    final public String githubUser = "tirsoalc";
+    final public String fullLink = "https://github.com/tirsoalc";
+
+    @Override
+    public String toString() {
+        return "tirsoalc{" +
+                "nome='" + nome + '\'' +
+                ", githubUser='" + githubUser + '\'' +
+                ", fullLink='" + fullLink + '\'' +
+                '}';
+    }
+}
