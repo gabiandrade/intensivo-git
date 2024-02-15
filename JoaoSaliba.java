49ba708a72f9598652bf5e26948f929a0cc714cb
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-02-15 20:31:59 -0300

    Create JoaoSaliba.java

---
+++ JoaoSaliba.java
@@ -0,0 +1,7 @@
+public class JoaoSaliba{
+ public static void main(String[] args){
+  System.out.println("Joao");
+  System.out.println("github: joaosaliba");
+  System.out.println("https://github.com/joaosaliba");
+  }
+}
