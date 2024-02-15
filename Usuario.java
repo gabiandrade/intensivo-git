75dac571f126cf6ca0d6337ffacaa0a0e8b52211
Moises Almeida <moisesalmeida.dev@gmail.com>
2024-02-15 19:31:27 -0400

    Criando classe usuário (Moises Almeida)

---
+++ Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario {
+    public String nome;
+    public String githubUser;
+
+    public Usuario() {
+        this.nome = "Moises Almeida";
+        this.githubUser = "moiseslx";
+    }
+}
