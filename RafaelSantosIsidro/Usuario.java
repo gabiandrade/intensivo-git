b71050c53b4040f0397f63d78f1c7a90fcd5929a
rafael-isidro <rafa-isidro@hotmail.com>
2024-02-15 20:47:46 -0300

    CrExercício Git - Rafael Isidro

---
+++ RafaelSantosIsidro/Usuario.java
@@ -0,0 +1,14 @@
+package RafaelSantosIsidro;
+
+public class Usuario {
+    private String nome = "Rafael Santos Isidro";
+    private String githubUser = "rafael-isidro";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGithubUser() {
+        return githubUser;
+    }
+}
